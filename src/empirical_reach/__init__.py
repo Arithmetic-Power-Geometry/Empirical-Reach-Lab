092ba7a2fc7436ed4b4c7dc787ddab65d17e0024
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-15 12:15:31 +0530

    Add package initializer

---
+++ src/empirical_reach/__init__.py
@@ -0,0 +1,11 @@
+from .core import Experiment, empirical_partition, unresolved_diameter, reach_gain, blackwell_dominates
+from .worlds import make_hidden_bit_world
+
+__all__ = [
+    "Experiment",
+    "empirical_partition",
+    "unresolved_diameter",
+    "reach_gain",
+    "blackwell_dominates",
+    "make_hidden_bit_world",
+]
